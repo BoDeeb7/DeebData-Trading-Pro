2ba2306cb6ca2e87fdf8044cb29764295798e1e9
BoDeeb7 <129641627+BoDeeb7@users.noreply.github.com>
2026-04-12 18:33:54 +0300

    Add files via upload

---
+++ src_app_manual-analysis_page_tsx_46ca598a._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_7290bcb7._.js",
+  "static/chunks/node_modules_e1b363f3._.js"
+],
+    source: "dynamic"
+});
